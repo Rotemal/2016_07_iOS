bf8eb008b96a245851bc7608a617bd00ac4a9a9e
Elad Lavi <eladlavi@Elads-MacBook-Pro.local>
2016-08-10 14:57:23 +0300

    Lesson 09

--- Lesson 09 - Navigation/Lesson 09 - Navigation/SecondViewController.swift
+++ Lesson 09 - Navigation/Lesson 09 - Navigation/SecondViewController.swift
@@ -10,31 +10,75 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    var nextStep:Int = 2;
+    var thirdViewController: ThirdViewController!;
+    
     override func viewDidLoad() {
         super.viewDidLoad();
         
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 30));
-        label.text = "Second";
-        label.center = view.center;
+        
+        view.backgroundColor = UIColor.whiteColor();
+        
+        let button1 = UIButton(type: .System);
+        button1.setTitle("go to screen 1", forState: .Normal);
+        button1.frame = CGRect(x: 5, y: view.frame.height - 50, width: 100, height: 30);
+        button1.addTarget(self, action: "btnGoToScreen1:", forControlEvents: .TouchUpInside);
+        view.addSubview(button1);
+        
+        let button2 = UIButton(type: .System);
+        button2.setTitle("go to screen 3", forState: .Normal);
+        button2.frame = CGRect(x: view.frame.width - 100 - 5, y: view.frame.height - 50, width: 100, height: 30);
+        button2.addTarget(self, action: "btnGoToScreen3:", forControlEvents: .TouchUpInside);
+        view.addSubview(button2);
+        
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 80, width: 300, height: 30));
+        label.text = "View Controller 2";
+        label.center.x = view.center.x;
         view.addSubview(label);
         
-        view.backgroundColor = UIColor.whiteColor();//otherwise it is transparent which causes a black screen.
-        let btnClose = UIButton(type: .System);
-        btnClose.frame = CGRect(x: label.frame.origin.x, y: label.frame.maxY + 10, width: label.frame.width, height: label.frame.height);
-        btnClose.setTitle("close", forState: .Normal);
-        btnClose.addTarget(self, action: "btnClose:", forControlEvents: .TouchUpInside);
-        view.addSubview(btnClose);
     }
     
-    func btnClose(sender: UIButton){
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated);
+        
+        
+        if(thirdViewController != nil){
+            thirdViewController.secondViewController = nil;
+        }
+        
+        switch nextStep {
+        case 1:
+            dismissViewControllerAnimated(true, completion: nil);
+        case 2:
+            break;
+        case 3:
+            goToThirdViewController();
+        default:
+            break;
+        }
+        
+        
+    }
+    
+    
+    func goToThirdViewController(){
+        if(thirdViewController == nil){
+            thirdViewController = ThirdViewController();
+        }
+        thirdViewController.secondViewController = self;
+        presentViewController(thirdViewController, animated: true, completion: nil);
+    }
+    
+    
+    func btnGoToScreen1(sender: UIButton){
         dismissViewControllerAnimated(true, completion: nil);
     }
     
-    
-    
-    
-    
-    
+    func btnGoToScreen3(sender: UIButton){
+        goToThirdViewController();
+    }
+
     
     
     
--- Lesson 09 - Navigation/Lesson 09 - Navigation/ViewController.swift
+++ Lesson 09 - Navigation/Lesson 09 - Navigation/ViewController.swift
@@ -13,21 +13,44 @@ class ViewController: UIViewController {
     var secondViewController: SecondViewController!;
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        let btnShowSecond = UIButton(type: .System);
-        btnShowSecond.frame = CGRect(x: 0, y: 50, width: 150, height: 30);
-        btnShowSecond.center.x = view.center.x;
-        btnShowSecond.setTitle("show second", forState: .Normal);
-        btnShowSecond.addTarget(self, action: "btnShowSecond:", forControlEvents: .TouchUpInside);
-        view.addSubview(btnShowSecond);
+        super.viewDidLoad();
         
-        secondViewController = SecondViewController();
+        let button1 = UIButton(type: .System);
+        button1.setTitle("go to screen 2", forState: .Normal);
+        button1.frame = CGRect(x: 5, y: view.frame.height - 50, width: 100, height: 30);
+        button1.addTarget(self, action: "btnGoToScreen2:", forControlEvents: .TouchUpInside);
+        view.addSubview(button1);
+        
+        let button2 = UIButton(type: .System);
+        button2.setTitle("go to screen 3", forState: .Normal);
+        button2.frame = CGRect(x: view.frame.width - 100 - 5, y: view.frame.height - 50, width: 100, height: 30);
+        button2.addTarget(self, action: "btnGoToScreen3:", forControlEvents: .TouchUpInside);
+        view.addSubview(button2);
+        
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 80, width: 300, height: 30));
+        label.text = "View Controller 1";
+        label.center.x = view.center.x;
+        view.addSubview(label);
+        
     }
     
-    func btnShowSecond(sender: UIButton){
+    
+    func btnGoToScreen2(sender: UIButton){
+        if(secondViewController == nil){
+            secondViewController = SecondViewController();
+        }
+        secondViewController.nextStep = 2;
         presentViewController(secondViewController, animated: true, completion: nil);
     }
     
+    func btnGoToScreen3(sender: UIButton){
+        if(secondViewController == nil){
+            secondViewController = SecondViewController();
+        }
+        secondViewController.nextStep = 3;
+        presentViewController(secondViewController, animated: true, completion: nil);
+    }
     
     
     
